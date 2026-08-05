d0ec8822ba5059fd1049e2868ecfb03d90eaca6a
alphasoft-in <alphasoftinnovation@gmail.com>
2026-08-05 09:54:20 -0500

    Update address and add Google Maps to About pages

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -45,7 +45,7 @@ export default function SiteFooter({ lang = 'en' }: Props) {
           <ul className="space-y-4 text-center md:text-left">
             <li className="flex flex-col md:flex-row items-center md:items-start space-y-2 md:space-y-0 md:space-x-3 text-gray-300 font-light text-sm">
               <svg className="w-5 h-5 text-[#dfa126] shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-              <span>Urb. Santa Rosa, Cusco, Peru</span>
+              <span>Calle Garcilaso 210 Int. Ofi. 124, Cusco, Peru</span>
             </li>
             <li className="flex flex-col md:flex-row items-center md:items-start space-y-2 md:space-y-0 md:space-x-3 text-gray-300 font-light text-sm">
               <svg className="w-5 h-5 text-[#dfa126] shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
